ae6ad29f8b3ca59e5f7e9399213b2fb0f65a1b42
bng18gh <116107377+bng18gh@users.noreply.github.com>
2024-06-02 11:57:33 -0700

    added winning and losing

--- Src/minesweeper.c
+++ Src/minesweeper.c
@@ -44,9 +44,13 @@ void flag(void);
 void unflag(void);
 void update_mines(void);
 void mine(int row, int col);
+void loss_screen(int row, int col);
+void expose_mines(char character);
+void score_points(int row, int col);
 
 
-int mines_left = 10;
+int mines_left;
+int spaces_exposed; //temporary
 static char game [9][9] = {{'*', '2', '1', '1', ' ', '1', '1', '2', '1'},
 						  	  	   {'1', '2', '*', '1', ' ', '1', '*', '2', '*'},
 									{' ', '1', '1', '1', ' ', '1', '1', '2', '1'},
@@ -77,7 +81,7 @@ void game_start(){
 	LPUART_Print("Press the x key to start");
 	LPUART_EscPrint(NONE); //stop blinking
 	set_cursor_position(1,1);
-	write_string(":) Mines Left:10");
+	write_string(":) Mines Left:--");
 	set_cursor_position(2,1);
 	write_string("Score: ---------");
 }
@@ -94,9 +98,10 @@ void game_start(){
 void board_init(){
 	LPUART_EscPrint(CLEAR);
 	LPUART_EscPrint(CURSOR_START); //(start of the board point)
+	LPUART_EscPrint(BLACK);
 	for (int i = 0; i < ROWS; i++){
 		for (int j = 0; j < COLUMNS; j++){
-			LPUART_PrintChar('▓'); //creates odd question mark
+			LPUART_PrintChar('▓'); //odd question mark (MAC), or black box (PC)
 		}
 		LPUART_EscPrint(DOWN);
 		LPUART_EscPrint("[9D");
@@ -109,9 +114,13 @@ void board_init(){
 	LPUART_Print("Flag: F   Unflag: U");
 	LPUART_EscPrint("[4;40H");
 	LPUART_Print("Mine: M");
+	set_cursor_position(1,15);
+	write_string("10");
 	set_cursor_position (2, 8);
 	write_string("000000000");
 	LPUART_EscPrint(CURSOR_START); //(start of the board point)
+	mines_left = 10;
+	spaces_exposed = 0;
 
 }
 
@@ -179,6 +188,99 @@ void update_mines(){
  * date     : 240531
  * -------------------------------------------------------------------------- */
 void mine(int row, int col){
+	if (game[row - 1][col - 1] == '*'){ //dug up a mine
+		loss_screen(row, col);
+	}
+	else{
+		score_points(row, col);
+	}
+}
+
+/* -----------------------------------------------------------------------------
+ * function : loss_screen( )
+ * INs      : current row and column position of the cursor
+ * OUTs     : none
+ * action   : screen and LCD update for when you lose the game
+ * authors  : Brandon Ng (bn) - bng18@calpoly.edu
+ * version  : 0.1
+ * date     : 240602
+ * -------------------------------------------------------------------------- */
+void loss_screen(int row, int col){
+	LPUART_EscPrint(CURSOR_START);
+	expose_mines('*');
+	//change the color for the actual mine you hit
+	LPUART_EscPrint(RED);
+	LPUART_EscPrint(CURSOR_START);
+	//go to the mine location
+	for (int i = 0; i < col - 1; i++){
+		LPUART_EscPrint(RIGHT);
+	}
+	for (int i = 0; i < row - 1; i++){
+		LPUART_EscPrint(DOWN);
+	}
 	LPUART_PrintChar(game[row - 1][col - 1]);
 	LPUART_EscPrint(LEFT);
+
+	//print the losing terminal and LCD screen
+	LPUART_EscPrint("[12;40H");
+	LPUART_Print("Too bad! You hit a mine.");
+	LPUART_EscPrint("[13;40H");
+	LPUART_EscPrint(BLINK);
+	LPUART_Print("Press x to restart the game");
+	LPUART_EscPrint(NONE);
+	set_cursor_position(1,2);
+	write_string("(");//sad face on the LCD
+
 }
+/* -----------------------------------------------------------------------------
+ * function : expose_mines( )
+ * INs      : none
+ * OUTs     : none
+ * action   : show all the mines on the board
+ * authors  : Brandon Ng (bn) - bng18@calpoly.edu
+ * version  : 0.1
+ * date     : 240602
+ * -------------------------------------------------------------------------- */
+void expose_mines(char character){
+	for (int i = 0; i < ROWS; i++){
+		for (int j = 0; j < COLUMNS; j++){
+			if (game[i][j] =='*'){
+				LPUART_PrintChar(character);
+			}
+			else{
+				LPUART_EscPrint(RIGHT);
+			}
+		}
+		LPUART_EscPrint(DOWN);
+		LPUART_EscPrint("[9D");
+	}
+}
+
+/* -----------------------------------------------------------------------------
+ * function : score_points( )
+ * INs      : current row and column position of the cursor
+ * OUTs     : none
+ * action   : screen and LCD update for completing a mine
+ * authors  : Brandon Ng (bn) - bng18@calpoly.edu
+ * version  : 0.1
+ * date     : 240602
+ * -------------------------------------------------------------------------- */
+void score_points(int row, int col){
+	//expose how many mines are around
+	LPUART_PrintChar(game[row - 1][col - 1]);
+	LPUART_EscPrint(LEFT);
+	spaces_exposed++;
+	//if you've mined all the correct things, win
+	if (spaces_exposed == 71){//9x9 grid - 10 mines
+		LPUART_EscPrint(CURSOR_START);
+		LPUART_EscPrint(GREEN);
+		expose_mines('>');
+		LPUART_EscPrint("[12;40H");
+		LPUART_Print("Yay! You won the game.");
+		LPUART_EscPrint("[13;40H");
+		LPUART_EscPrint(BLINK);
+		LPUART_Print("Press x to make a new game");
+		LPUART_EscPrint(NONE);
+	}
+}
+
