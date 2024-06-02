fee383b5b4132c1bdc4e0ff5e8db4bb7270a6e64
bng18gh <116107377+bng18gh@users.noreply.github.com>
2024-06-02 16:56:58 -0700

    Create timer.h

---
+++ Inc/timer.h
@@ -0,0 +1,26 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : timer.h
+  * @brief          : Header for timer.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+#ifndef INC_TIMER_H_
+#define INC_TIMER_H_
+
+void timer_init(void);
+
+#endif /* INC_TIMER_H_ */
